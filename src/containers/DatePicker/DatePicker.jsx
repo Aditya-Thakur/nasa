e5309e63351a36f797d78a337053eb6a4b3d1f39
aditya-thakur <adityathakur532@gmail.com>
2021-12-24 13:27:16 +0530

    datePicker

--- src/containers/DatePicker/DatePicker.jsx
+++ src/containers/DatePicker/DatePicker.jsx
@@ -1,14 +1,37 @@
+import { TextField } from '@material-ui/core'
 import React from 'react'
+import { useState } from 'react'
+import { feed } from '../../services/info.service'
 import './DatePicker.css'
 export default function DatePicker() {
+    let dateRange = {
+        startDate: new Date().toDateString,
+        endDate: new Date().toDateString
+    }
+
     return (
         <div className="card">
             <div className="card-title">
                 Search by date range
             </div>
             <div className="card-body">
-                <input type="date" name="Start Date" id="" />
-                <input type="date" name="End Date" id="" />
+                <p>Start Date:</p>
+                {/* <TextField value={dateRange.startDate} id='startDate' label="Start Date" type="date" onChange={ event => {
+                    dateRange.startDate = event.target.value
+                }}  /> */}
+                <input type="date" name="Start_Date" id="" value={dateRange.startDate} onChange={ event => {
+                    dateRange.startDate = event.target.value
+                }}  />
+                <p>End Date:</p>
+                <input type="date" name="End_Date" id="" value={dateRange.endDate} onChange={ event => {
+                    dateRange.startDate = event.target.value
+                }}  />
+                <button onClick={
+                    event => {
+                        event.preventDefault();
+                        feed(dateRange)
+                    }
+                    }>Search</button>
             </div>
         </div>
     )
